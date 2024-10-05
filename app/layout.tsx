410ed08a762bd51502820b8accfab12b6f118363
VeygaX <84923630+veygax@users.noreply.github.com>
2024-10-05 18:19:56 +0000

    fixed favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.ico" sizes="any" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
