744346f3f809caa6c0374d548921fcf34897ca2a
VeygaX <84923630+veygax@users.noreply.github.com>
2025-06-20 16:26:15 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-6575709505321532">
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
